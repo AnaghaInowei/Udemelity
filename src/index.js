d74aeb4ac0f27c0998d1eacb4f7c5710b0d49b73
Ino Tech <ezekieldavid150@gmail.com>
2026-06-09 06:37:31 +0100

    feat: configure Tailwind CSS and implement interactive UI components and scroll-triggered animations

--- src/index.js
+++ src/index.js
@@ -172,3 +172,6 @@ document.addEventListener('DOMContentLoaded', () => {
 // const canvas = document.getElementById('canvas3d');
 // const app = new Application(canvas);
 // app.load('https://prod.spline.design/NjQiOmC6aGguUAh1/scene.splinecode');
+
+
+
